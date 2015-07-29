08c6cde1e6cdcd7e71e57712beb936e0bf04baa8
Co-Pierre Georg <cogeorg@gmail.com>
2015-07-29 14:53:51 +0100

    Worked on stringtools to make them compliant with PEP8

--- stringtools.py
+++ stringtools.py
@@ -3,39 +3,32 @@
 
 __author__ = """Michael E. Rose (Michael.Ernst.Rose@gmail.com)"""
 
+import unicodedata
 
-#-------------------------------------------------------------------------
-#
-#  stringtools.py is a collection of tools to manipulate strings
-#
-#-------------------------------------------------------------------------
 
-if __name__ == '__main__':
-
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 #
 #  class DirtyString
 #
-#-------------------------------------------------------------------------
-    class DirtyString(object):
-        __version__ = 0.1
+# -------------------------------------------------------------------------
+class DirtyString(object):
+    __version__ = 0.11
 
     #
     #  METHODS
     #
-
-    #-------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # standardize(aDirtyString)
-    #-------------------------------------------------------------------------
-def standardize(aDirtyString):
+    # -------------------------------------------------------------------------
+    def standardize(self, a_dirty_string):
         """
         Removes interpunctuation and blanks from a string
 
         Args:
-            aDirtyString (str) -- the string to be cleaned
+            a_dirty_string (str) -- the string to be cleaned
 
         Returns:
-            aCleanString (str) -- the string without interpunctuation
+            a_clean_string (str) -- the string without interpunctuation
 
         Note:
             The use of normalize() is encouraged, as standardize() only removes
@@ -43,71 +36,66 @@ def standardize(aDirtyString):
         """
 
         # Remove ASCII Punctuation & Symbols
-        aCleanerString = "".join(i for i in aDirtyString if not 32<=ord(i)<=47)
+        a_cleaner_string = "".join(i for i in a_dirty_string if not 32<=ord(i)<=47)
 
         # Remove ASCII Punctuation & Symbols, part I
-        aCleanerString = "".join(i for i in aCleanerString if not 58<=ord(i)<=64)
+        a_cleaner_string = "".join(i for i in a_cleaner_string if not 58<=ord(i)<=64)
 
         # Remove ASCII Punctuation & Symbols, part II
-        aCleanerString = "".join(i for i in aCleanerString if not 91<=ord(i)<=96)
+        a_cleaner_string = "".join(i for i in a_cleaner_string if not 91<=ord(i)<=96)
 
         # Remove ASCII Punctuation & Symbols, part III
-        aCleanString = "".join(i for i in aCleanerString if not 123<=ord(i)<=126)
+        a_clean_string = "".join(i for i in a_cleaner_string if not 123<=ord(i)<=126)
 
-        return aCleanString
-    #-------------------------------------------------------------------------
+        return a_clean_string
+    # -------------------------------------------------------------------------
 
-
-    #-------------------------------------------------------------------------
-    # normalize(aDirtyString)
-    #-------------------------------------------------------------------------
-def normalize(aDirtyString):
+    # -------------------------------------------------------------------------
+    # normalize(a_dirty_string)
+    # -------------------------------------------------------------------------
+    def normalize(self, a_dirty_string):
         """
         Returns the normal form of a Unicode string (transliteration). Removes
         characters in case transliteration is not possible.
 
         Args:
-            aDirtyString (str) -- the string to be cleaned
+            a_dirty_string (str) -- the string to be cleaned
 
         Returns:
-            aCleanString (str) -- the string without interpunctuation
+            a_clean_string (str) -- the string without interpunctuation
         """
-
-        import unicodedata
-
         # Replace by ASCII equivalent
         try:
-            aCleanString = ''.join(c for c in unicodedata.normalize('NFKD', unicode(aDirtyString))
-                            if unicodedata.category(c) != 'Mn')
+            a_clean_string = ''.join(c for c in unicodedata.normalize('NFKD', unicode(a_dirty_string))
+                                     if unicodedata.category(c) != 'Mn')
 
         # Remove if not possible
         except UnicodeDecodeError:
-            aCleanString = "".join(i for i in aDirtyString if ord(i)<128)
+            a_clean_string = "".join(i for i in a_dirty_string if ord(i)<128)
 
-        return aCleanString
-    #-------------------------------------------------------------------------
+        return a_clean_string
+    # -------------------------------------------------------------------------
 
-
-    #-------------------------------------------------------------------------
-    # translit_nordic(aDirtyString)
-    #-------------------------------------------------------------------------
-def translit_nordic(aDirtyString):
+    # -------------------------------------------------------------------------
+    # translit_nordic(a_dirty_string)
+    # -------------------------------------------------------------------------
+    def translit_nordic(self, a_dirty_string):
         """
         Replaces characters used in nordic languages not covered by normalize()
 
         Args:
-            aDirtyString (str) -- the string to be cleaned
+            a_dirty_string (str) -- the string to be cleaned
 
         Returns:
-            aCleanString (str) -- the string without interpunctuation
+            a_clean_string (str) -- the string without interpunctuation
         """
         # Some characters are out of range
-        aCleanerString = aDirtyString.replace("Ø","O").replace("ø", "o")
-        aCleanerString = aCleanerString.replace("Æ","AE").replace("æ", "ae")
-        aCleanerString = aCleanerString.replace("Ð","D").replace("ð", "d")
-        aCleanerString = aCleanerString.replace("Þ","TH").replace("þ", "th")
-        aCleanerString = aCleanerString.replace("Œ","OE").replace("œ", "oe")
-        aCleanString = aCleanerString.replace("ß","sz").replace("ƒ", "f")
+        a_cleaner_string = a_dirty_string.replace("Ø","O").replace("ø", "o")
+        a_cleaner_string = a_cleaner_string.replace("Æ","AE").replace("æ", "ae")
+        a_cleaner_string = a_cleaner_string.replace("Ð","D").replace("ð", "d")
+        a_cleaner_string = a_cleaner_string.replace("Þ","TH").replace("þ", "th")
+        a_cleaner_string = a_cleaner_string.replace("Œ","OE").replace("œ", "oe")
+        a_clean_string = a_cleaner_string.replace("ß","sz").replace("ƒ", "f")
 
-        return aCleanString
-    #-------------------------------------------------------------------------
+        return a_clean_string
+    # -------------------------------------------------------------------------

19eb5ef45725e71f00bec156870592a6a82b3f02
Co-Pierre Georg <cogeorg@gmail.com>
2015-07-29 14:53:35 +0100

    Added a new routine to conftools to read structured xml data instead of only parameter files

--- conftools.py
+++ conftools.py
@@ -6,11 +6,11 @@ __author__ = """Co-Pierre Georg (co-pierre.georg@uct.ac.za)"""
 import sys
 import logging
 
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 #
 #  class Config
 #
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 class Config(object):
     __version__ = 0.9
 
@@ -24,33 +24,23 @@ class Config(object):
     #
     #  METHODS
     #
-    #-------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     #  __init__
-    #-------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     def __init__(self):
         pass
 
-    #-------------------------------------------------------------------------
-    # read_xml_config_file
-    #-------------------------------------------------------------------------
-    def read_xml_config_file(self, config_file_name):
-        from xml.etree import ElementTree
-        xmlText = open(config_file_name).read()
 
-        element = ElementTree.XML(xmlText)
-        self.identifier = element.attrib['identifier']
 
-        # loop over all entries in the xml file
-        for subelement in element:
-            name = subelement.attrib['name']
-            try:  # we see whether the value is a float
-                value = float(subelement.attrib['value'])
-            except:  # if not, it is a string
-                value = str(subelement.attrib['value'])
-            if (subelement.attrib['type'] == 'static'):
-                self.static_parameters[name] = value
-            if (subelement.attrib['type'] == 'changing'):
-                valid_from = subelement.attrib['validity'].rsplit("-")[0]
-                valid_to = subelement.attrib['validity'].rsplit("-")[1]
-                self.changing_parameters[name] = [value, valid_from, valid_to]
-    #-------------------------------------------------------------------------
+
+#            name = subelement.attrib['name']
+#            try:  # we see whether the value is a float
+#                value = float(subelement.attrib['value'])
+#            except:  # if not, it is a string
+#                value = str(subelement.attrib['value'])
+#            if (subelement.attrib['type'] == 'static'):
+#                self.static_parameters[name] = value
+#            if (subelement.attrib['type'] == 'changing'):
+#                valid_from = subelement.attrib['validity'].rsplit("-")[0]
+#                valid_to = subelement.attrib['validity'].rsplit("-")[1]
+#                self.changing_parameters[name] = [value, valid_from, valid_to]

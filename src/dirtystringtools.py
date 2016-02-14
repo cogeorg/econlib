0c45cc63fd066dd06217267f9376bcd95114bc63
Michael-E-Rose <Michael.Ernst.Rose@gmail.com>
2016-02-14 18:37:44 +0100

    Improve documentation and layout (#3)

--- src/dirtystringtools.py
+++ src/dirtystringtools.py
@@ -1,116 +1,96 @@
-#!/usr/bin/env python
-# -*- coding: utf-8
+"""
+================
+dirtystringtools
+================
 
-__author__ = """Michael E. Rose (Michael.Ernst.Rose@gmail.com)"""
-
+A collection of methods for cleaning strings
+"""
 import unicodedata
+__author__  =  """Michael E. Rose (Michael.Ernst.Rose@gmail.com)"""
+__all__ = ['standardize', 'normalize', 'translit_nordic']
+__version__ = 0.13
 
 
-# -------------------------------------------------------------------------
-#
-#  class DirtyString
-#
-# -------------------------------------------------------------------------
-class DirtyString(object):
-    __version__ = 0.12
+def standardize(self, a_dirty_string):
+    """
+    Removes interpunctuation and blanks from a string.
 
-    #
-    #  METHODS
-    #
-    # -------------------------------------------------------------------------
-    # standardize(aDirtyString)
-    # -------------------------------------------------------------------------
-    def standardize(self, a_dirty_string):
-        """
-        Removes interpunctuation and blanks from a string
+    Parameters
+    ----------
+    a_dirty_string: the string to be cleaned (str)
 
-        Args:
-            a_dirty_string (str) -- the string to be cleaned
+    Returns
+    -------
+    a string without interpunctuation
 
-        Returns:
-            a_clean_string (str) -- the string without interpunctuation
+    Note
+    ----
+    The use of normalize() is encouraged, as standardize() only removes
+    characters until ordinal number 128.
+    """
+    # Remove extra whitespaces
+    while "  " in a_dirty_string:
+        a_dirty_string = a_dirty_string.replace("  ", " ")
+    # replace _ with whitespace to keep syntactic structure of string
+    a_dirty_string = a_dirty_string.replace("_", " ")
 
-        Note:
-            The use of normalize() is encouraged, as standardize() only removes
-            unicode characters until ordinal number 128
-        """
+    # Remove ASCII Punctuation & Symbols
+    # Note: we keep whitespaces because we want to keep the syntactic structure of the string
+    a_cleaner_string = "".join(i for i in a_dirty_string if not 33 <= ord(i) <= 47)
 
-        # Remove extra whitespaces
-        while "  " in a_dirty_string:
-            a_dirty_string = a_dirty_string.replace("  ", " ")
-        # replace _ with whitespace to keep syntactic structure of string
-        a_dirty_string = a_dirty_string.replace("_", " ")
+    # Remove ASCII Punctuation & Symbols, part I
+    a_cleaner_string = "".join(i for i in a_cleaner_string if not 58<=ord(i)<=64)
 
-        # Remove ASCII Punctuation & Symbols
-        # Note: we keep whitespaces because we want to keep the syntactic structure of the string
-        a_cleaner_string = "".join(i for i in a_dirty_string if not 33 <= ord(i) <= 47)
+    # Remove ASCII Punctuation & Symbols, part II
+    a_cleaner_string = "".join(i for i in a_cleaner_string if not 91<=ord(i)<=96)
 
-        # Remove ASCII Punctuation & Symbols, part I
-        a_cleaner_string = "".join(i for i in a_cleaner_string if not 58<=ord(i)<=64)
+    # Remove ASCII Punctuation & Symbols, part III
+    a_clean_string = "".join(i for i in a_cleaner_string if not 123<=ord(i)<=126)
 
-        # Remove ASCII Punctuation & Symbols, part II
-        a_cleaner_string = "".join(i for i in a_cleaner_string if not 91<=ord(i)<=96)
+    # Change whitespace to underscore
+    a_clean_string = a_clean_string.replace(" ", "_")
 
-        # Remove ASCII Punctuation & Symbols, part III
-        a_clean_string = "".join(i for i in a_cleaner_string if not 123<=ord(i)<=126)
+    return a_clean_string
 
-        # Change whitespace to underscore
-        a_clean_string = a_clean_string.replace(" ", "_")
 
-        return a_clean_string
-    # -------------------------------------------------------------------------
+def normalize(self, a_dirty_string):
+    """
+    Returns the normal form of a Unicode string (transliteration). Removes
+    characters in case transliteration is not possible.
 
-    # -------------------------------------------------------------------------
-    # normalize(a_dirty_string)
-    # -------------------------------------------------------------------------
-    def normalize(self, a_dirty_string):
-        """
-        Returns the normal form of a Unicode string (transliteration). Removes
-        characters in case transliteration is not possible.
+    Args:
+        a_dirty_string (str) -- the string to be cleaned
 
-        Args:
-            a_dirty_string (str) -- the string to be cleaned
+    Returns:
+        a_clean_string (str) -- the string without interpunctuation
+    """
+    try: # Replace by ASCII equivalent
+        a_clean_string = ''.join(c for c in unicodedata.normalize('NFKD', unicode(a_dirty_string))
+                                 if unicodedata.category(c) != 'Mn')
+    except UnicodeDecodeError: # Remove if not possible
+        a_clean_string = "".join(i for i in a_dirty_string if ord(i)<128)
+    return a_clean_string
 
-        Returns:
-            a_clean_string (str) -- the string without interpunctuation
-        """
-        # Replace by ASCII equivalent
-        try:
-            a_clean_string = ''.join(c for c in unicodedata.normalize('NFKD', unicode(a_dirty_string))
-                                     if unicodedata.category(c) != 'Mn')
 
-        # Remove if not possible
-        except UnicodeDecodeError:
-            a_clean_string = "".join(i for i in a_dirty_string if ord(i)<128)
+def translit_nordic(self, ascii_string):
+    """
+    Replaces characters used in nordic languages. These characters are not
+    in UTF-8 and will get lost when decoding. This is only meaningfull
+    in python 2.
 
-        return a_clean_string
-    # -------------------------------------------------------------------------
+    Parameters
+    ascii_string: the string to be cleaned, ascii formatted (str)
 
-    # -------------------------------------------------------------------------
-    # translit_nordic(ascii_string)
-    # -------------------------------------------------------------------------
-    def translit_nordic(self, ascii_string):
-        """
-        Replaces characters used in nordic languages. These characters are not
-        in UTF-8 and will get lost when decoding. This is only meaningfull
-        in python 2.
-
-        Args:
-            ascii_string (str) -- the string to be cleaned, ascii formatted
-
-        Returns:
-            a_clean_string (str) -- the string without replaced characters
-        """
-        
-        if not isinstance(ascii_string, str):
-            raise ValueError('Expects an ascii encoded string')
-
-        a_cleaner_string = ascii_string.replace('\xc3\x98','O').replace('ø', 'o')
-        a_cleaner_string = a_cleaner_string.replace('\xc3\x86','AE').replace('\xc3\xa6', 'ae')
-        a_cleaner_string = a_cleaner_string.replace('\xc3\x90','D').replace('\xc3\xb0', 'd')
-        a_cleaner_string = a_cleaner_string.replace('\xc3\x9e','TH').replace('\xc3\xbe', 'th')
-        a_cleaner_string = a_cleaner_string.replace('\xc5\x92','OE').replace('\xc5\x93', 'oe')
-        a_clean_string = a_cleaner_string.replace('\xc3\x9f','sz').replace('\xc6\x92', 'f')
-
-        return a_clean_string
-    # -------------------------------------------------------------------------
+    Returns
+    -------
+    a_clean_string: the string without replaced characters (str)
+    """
+    if not isinstance(ascii_string, str):
+        raise ValueError('Expects an ascii encoded string')
+    a_cleaner_string = ascii_string.replace('\xc3\x98','O').replace('ø', 'o')
+    a_cleaner_string = a_cleaner_string.replace('\xc3\x86','AE').replace('\xc3\xa6', 'ae')
+    a_cleaner_string = a_cleaner_string.replace('\xc3\x90','D').replace('\xc3\xb0', 'd')
+    a_cleaner_string = a_cleaner_string.replace('\xc3\x9e','TH').replace('\xc3\xbe', 'th')
+    a_cleaner_string = a_cleaner_string.replace('\xc5\x92','OE').replace('\xc5\x93', 'oe')
+    a_clean_string = a_cleaner_string.replace('\xc3\x9f','sz').replace('\xc6\x92', 'f')
+    return a_clean_string
--- src/iotools.py
+++ src/iotools.py
@@ -1,145 +1,92 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""
+=======
+iotools
+=======
 
+A collection of methods for input and outputting data
+"""
+import csv
 __author__ = """Michael E. Rose (Michael.Ernst.Rose@gmail.com)"""
+__all__ = ['csv_to_dict', 'csv_to_nested_dict', 'nested_dict_to_csv']
+__version__ = 0.3
+
+def csv_to_dict(filename,key_func,value_func,skip_header=True):
+    """
+    Reads a csv as a dictionary, where values and keys are set
+    in a flexible manner. Delimiters are determined automatically.
+
+    Parameters
+    ----------
+    filename: the name of the source file (str)
+    key_func: a function specifying which row(s) to use as dictionary key (func)
+    value_func: a function specifiying which row(s) to use as dictionary values (func)
+    skip_header: whether the header should be skpped (boolean - p)
+
+    Returns
+    -------
+    dictionary object
+    """
+    aDict = {}
+    with open(filename, 'r') as f:
+        dialect = csv.Sniffer().sniff(aFile.readline())
+        f.seek(0)
+        csvReader = csv.reader(f, dialect=dialect)
+        if skip_header: next(csvReader, None)
+        for row in csvReader:
+            key = key_func(row)
+            aDict[key] = value_func(row)
+    return aDict
 
 
-#-------------------------------------------------------------------------
-#
-#  iotools.py is a collection of tools for input and outputting data
-#
-#-------------------------------------------------------------------------
+def csv_to_nested_dict(filename,key_func,ordered=False):
+    """
+    Reads a csv as a dictionary of dictionaries, where keys are set
+    in a flexible manner. Delimiters are determined automatically.
 
+    Parameters
+    ----------
+    filename: the name of the source file (str)
+    key_func: a function specifying which row(s) to use as dictionary key (func)
+    ordered: whether to return a nested dict instead of an ordinary dict (boolean -o)
 
-#-------------------------------------------------------------------------
-# getDialect(aFile)
-#-------------------------------------------------------------------------
-def getDialect(aFile):
-
-    return csv.Sniffer().sniff(aFile.readline())
-#-------------------------------------------------------------------------
-
-#
-#  METHODS
-#
-
-#-------------------------------------------------------------------------
-# csv_to_dict(
-#    filename,
-#    key_func,
-#    value_func,
-#    skip_header
-#    )
-#-------------------------------------------------------------------------
-def csv_to_dict(filename,
-                key_func,
-                value_func,
-                skip_header = True):
-        """
-        Reads a csv as a dictionary, where values and keys are set in a flexible manner. Delimiters are determined automatically.
-
-        Args:
-            filename (str) -- the name of the source file
-            key_func (func) -- a function specifying which row(s) of the csv file should be the keys of the dictionary
-            value_func (func) -- a function specifiying which row(s) of the csv file should be the values of the dictionary
-            skip_header (boolean - optional) -- set to False if header should not be skipped
-
-        Returns:
-            aDict (dict) -- the dictionary compiled from the csv file
-
-        """
-
-        import csv
-
-        aDict = {}
-        with open(filename, 'r') as f:
-            csvDialect = getDialect(f)
-            f.seek(0)
-            csvReader = csv.reader(f, dialect=csvDialect)
-            if skip_header:
-                next(csvReader, None)
-            for row in csvReader:
-                key = key_func(row)
-                aDict[key] = value_func(row)
-
-        return aDict
-#-------------------------------------------------------------------------
-
-
-#-------------------------------------------------------------------------
-# csv_to_nested_dict(
-#    filename,
-#    key_func,
-#    skip_header
-#    )
-#-------------------------------------------------------------------------
-def csv_to_nested_dict(filename,
-                       key_func,
-                       ordered = False):
-        """
-        Reads a csv as a dictionary of dictionaries, where keys are set in a flexible manner. Delimiters are determined automatically.
-
-        Args:
-            filename (str) -- the name of the source file
-            key_func (func) -- a function specifying which row(s) of the csv file should be the keys of the dictionary
-            ordered (boolean - optional) -- set to True if entire dictionary shall be ordered
-
-        Returns:
-            aDict (dict) -- the nested dictionary compiled from the csv file
-
-        """
-
-        import csv
-
+    Returns
+    -------
+    dictionary object or ordered dictionary object
+    """
+    with open(filename, 'r') as f:
+        dialect = csv.Sniffer().sniff(aFile.readline())
+        f.seek(0)
+        csvReader = csv.reader(f, dialect=dialect)
         if ordered:
             from collections import OrderedDict
             aDict = OrderedDict()
-            with open(filename, 'rb') as f:
-                csvDialect = getDialect(f)
-                f.seek(0)
-                csvReader = csv.reader(f, dialect=csvDialect)
-                fields = next(csvReader)
-                for row in csvReader:
-                    temp = OrderedDict(zip(fields, row))
-                    key = key_func(temp)
-                    aDict[key] = temp
-
+            fields = next(csvReader)
+            for row in csvReader:
+                temp = OrderedDict(zip(fields, row))
+                key = key_func(temp)
+                aDict[key] = temp
         else:
             aDict = {}
-            with open(filename, 'rb') as f:
-                csvDialect = getDialect(f)
-                f.seek(0)
-                csvReader = csv.DictReader(f, dialect=csvDialect)
-                for row in csvReader:
-                    key = key_func(row)
-                    aDict[key] = row
-
-        return aDict
-#-------------------------------------------------------------------------
+            for row in csvReader:
+                key = key_func(row)
+                aDict[key] = row
+    return aDict
 
 
-#-------------------------------------------------------------------------
-# nested_dict_to_csv(
-#    nested_dict,
-#    file_name,
-#    fields
-#    )
-#-------------------------------------------------------------------------
 def nested_dict_to_csv(nested_dict,file_name,fields='',header=True):
-        """
-        Printes a nested dictionary to csv.
+    """
+    Prints a nested dictionary to csv.
 
-        Args:
-            nested_dict (dict) -- a dictionary of dictionaries
-            file_name (str) -- the name of output file
-            fields (list of str) -- the ordering of subkeys with the first entry being the main key
-            header (boolean - optional) -- set to False if header should not be written
-        """
-        import csv
-        with open(file_name,'w') as f:
-            w = csv.DictWriter(f, fieldnames=fields)
-            if header:
-                w.writeheader()
-            for k in nested_dict:
-                w.csvWriter({field: out_dict[k].get(field) or k for field in fields})
-#-------------------------------------------------------------------------
+    Parameters
+    ----------
+    nested_dict: a dictionary of dictionaries (dictionary object)
+    file_name: the name of output file (str)
+    fields: the ordering of subkeys with the first entry being the main key (list of str)
+    header: set to False if header should not be written (boolean - o)
+    """
+    with open(file_name,'w') as f:
+        w = csv.DictWriter(f, fieldnames=fields)
+        if header:
+            w.writeheader()
+        for k in nested_dict:
+            w.csvWriter({field: out_dict[k].get(field) or k for field in fields})
